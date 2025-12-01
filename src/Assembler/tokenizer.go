ec2b8a796f6b5f3f64d4fc7589c73c4235882b4b
Marcelo <marceloluis257gh@gmail.com>
2025-12-01 15:21:38 -0300

    tokenizer

--- src/Assembler/tokenizer.go
+++ src/Assembler/tokenizer.go
@@ -1,9 +1,56 @@
 package assembler
 
+var INSTRUCTIONS_SET = map[string]bool{
+	"add":     true,
+	"addi":    true,
+	"sub":     true,
+	"subi":    true,
+	"mult":    true,
+	"multi":   true,
+	"div":     true,
+	"divi":    true,
+	"move":    true,
+	"or":      true,
+	"ori":     true,
+	"and":     true,
+	"andi":    true,
+	"sll":     true,
+	"srl":     true,
+	"slt":     true,
+	"slti":    true,
+	"li":      true,
+	"syscall": true,
+	"j":       true,
+	"jr":      true,
+	"jal":     true,
+	"beq":     true,
+	"bne":     true,
+	"bgt":     true,
+	"bge":     true,
+	"blt":     true,
+	"ble":     true,
+	"return":  true,
+	"lw":      true,
+	"lb":      true,
+	"sw":      true,
+	"sb":      true,
+	"lv":      true,
+	"sv":      true,
+	"lrw":     true,
+	"lrb":     true,
+	"inc":     true,
+	"dec":     true,
+	".text":   true,
+	".data":   true,
+	"la":      true,
+	"rand":    true,
+}
+
 type TokenType int
 
 const (
-	INSTRUCTION TokenType = iota
+	IDENTIFIER TokenType = iota
+	INSTRUCTION
 	REGISTER
 	LABEL_DEF
 	LABEL_REF
@@ -34,11 +81,19 @@ type Tokenizer struct {
 	position int
 	column   int
 	line     int
+	len      int
+	tokens   []Token
 }
 
 func newTokenizer(data string) *Tokenizer {
-	tokenizer := Tokenizer{data, 0, 0, 0}
+	tokenizer := Tokenizer{data, 0, 0, 0, 0, []Token{}}
 	return &tokenizer
+}
+
+func (tokenizer *Tokenizer) addToken(token_type TokenType, value string) {
+	token := newToken(token_type, value, tokenizer.column, tokenizer.line)
+	tokenizer.tokens = append(tokenizer.tokens, *token)
+	tokenizer.len += 1
 }
 
 func (tokenizer *Tokenizer) advance() {
@@ -61,8 +116,40 @@ func (tokenizer *Tokenizer) getCurrentChar() byte {
 	return tokenizer.Data[tokenizer.position]
 }
 
+func (tokenizer *Tokenizer) handleSpace() {
+	tokenizer.advance()
+}
+
+func (tokenizer *Tokenizer) handleNewLine() {
+	tokenizer.addToken(NEW_LINE, "\n")
+	tokenizer.nextLine()
+}
+
+func isValidCharacterToIdentifier(s byte) bool {
+	matched := (s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z') || s == '_' || (s >= '0' && s <= '9')
+	return matched
+}
+
+func (tokenizer *Tokenizer) handleIdentifier() {
+	start := tokenizer.position
+
+	for isValidCharacterToIdentifier(tokenizer.getCurrentChar()) {
+		tokenizer.advance()
+	}
+
+	identifier := tokenizer.Data[start:tokenizer.position]
+	tokenizer.addToken(IDENTIFIER, identifier)
+}
+
 func (tokenizer *Tokenizer) Tokenize() {
 	for tokenizer.position != len(tokenizer.Data) {
-		tokenizer.advance()
+		current := tokenizer.getCurrentChar()
+		if current == ' ' {
+			tokenizer.handleSpace()
+		} else if current == '\n' {
+			tokenizer.handleNewLine()
+		} else if (current >= 'a' && current <= 'z') || (current >= 'A' && current <= 'Z') || current == '_' {
+			tokenizer.handleIdentifier()
+		}
 	}
 }
